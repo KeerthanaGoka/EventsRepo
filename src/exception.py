80a0333000b638c461a47b50cbc402853d1b5e22
Keerthana Goka <gkeerthana.sai@gmail.com>
2025-06-19 15:54:10 -0400

    data files

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 def error_message_detail(error, error_detail:sys):
    
     _,_,exc_tb=error_detail.exc_info()
@@ -16,7 +16,7 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-
+'''
 if __name__=="__main__":
 
     try:
@@ -24,3 +24,5 @@ if __name__=="__main__":
     except Exception as e:
         logging.info("divide by zero")
         raise CustomException(e,sys)
+
+'''
